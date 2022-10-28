4ffff4c3c9bf8227a7edf0d8ad012be0089d8181
surarim <surarim@yandex.ru>
2022-10-28 14:34:22 +0700

    Make result into file

--- vpnusers.py
+++ vpnusers.py
@@ -5,6 +5,9 @@
 #------------------------------------------------------------------------------------------------
 
 import os, sys
+openvpn_status_file = '/etc/openvpn/openvpn-status.log'
+openvpn_log_file = '/var/log/openvpn.log'
+result_log_file = '/var/log/vpnusers.log'
 
 #------------------------------------------------------------------------------------------------
 
@@ -12,16 +15,17 @@ import os, sys
 def run():
   users = []
   # Получение списка ip адресов пользователей и времени их сеанса (текущие подключения)
-  if os.path.isfile('/etc/openvpn/openvpn-status.log'):
-    for line in list(open('/etc/openvpn/openvpn-status.log')):
+  if os.path.isfile(openvpn_status_file):
+    for line in list(open(openvpn_status_file)):
       if line.find('ROUTING') != -1: break;
       if line.find('UNDEF') != -1:
         users.append(' ' + ((line.split(',')[1]).split(':')[0]) + ' ' + (line.split(',')[4]).replace("\n",""))
+    else: sys.exit(0)
 
   # Получение имён пользователей по их ip адресу из лога
   i = len(users)
-  if os.path.isfile('/var/log/openvpn.log'):
-    for line in reversed(list(open('/var/log/openvpn.log'))):
+  if os.path.isfile(openvpn_log_file):
+    for line in reversed(list(open(openvpn_log_file))):
       if i == 0: break;
       if line.find('TLS: Username/Password authentication succeeded') != -1:
         log_ip = ((line.split(' ')[2]).split(':')[0])
@@ -30,10 +34,12 @@ def run():
             i = i - 1
             users[num] = (line.split(' ')[9]).replace("'",'') + users[num]
             break
+    else: sys.exit(0)
 
   # Вывод результата
-  for num in range(len(users)):
-    print(users[num])
+  with open(result_log_file,'w') as logfile:
+    for num in range(len(users)):
+      logfile.write(users[num]+'\n')
 
 #------------------------------------------------------------------------------------------------
 
