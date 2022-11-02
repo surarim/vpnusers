baa56cd968581dcf771896409920feec44632ce1
surarim <surarim@yandex.ru>
2022-11-02 10:35:38 +0700

    Fix string bugs

--- vpnusers.py
+++ vpnusers.py
@@ -30,16 +30,17 @@ def run():
       if line.find('TLS: Username/Password authentication succeeded') != -1:
         log_ip = ((line.split(' ')[2]).split(':')[0])
         for num in range(len(users)):
-          if log_ip == users[num].split(' ')[1]:
+          if log_ip == users[num].split(' ')[1] and users[num].split(' ')[0] == '':
             i = i - 1
             users[num] = (line.split(' ')[9]).replace("'",'') + users[num]
             break
     else: sys.exit(0)
 
-  # Вывод результата
+  # Вывод результата в лог
   with open(result_log_file,'w') as logfile:
     for num in range(len(users)):
-      logfile.write(users[num]+'\n')
+      if users[num].split(' ')[0] != '':
+        logfile.write(users[num]+'\n')
 
 #------------------------------------------------------------------------------------------------
 
